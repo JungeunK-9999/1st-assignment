0cec9389b9a809271ea614168a8b05fbef978174
Jung <jungeun.lak@gmail.com>
2019-09-14 03:37:46 +0900

    두 번째 문제 끝

--- second.js
+++ second.js
@@ -2,15 +2,9 @@
 // 은행 예금 이자 구하기
 // node second.js 해보면 input output이 어떻게 되는지 알 수 있음
 
-const readline = require("readline");
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
+const line='213';
+const a=parseInt(line/(10**len));
 
-rl.on("line", function(line) {
-	console.log(line);
-	rl.close();
-}).on("close", function() {
-	process.exit();
-});
+
+
+console.log(a);
