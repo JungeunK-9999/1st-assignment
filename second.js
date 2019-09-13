c4fb4c0dbf268f72cf06643d6837a3de3a7a3efd
Jung <jungeun.lak@gmail.com>
2019-09-14 03:43:12 +0900

    두 번째 과제 진짜 끝// 저장을 잘 하자

--- second.js
+++ second.js
@@ -2,9 +2,25 @@
 // 은행 예금 이자 구하기
 // node second.js 해보면 input output이 어떻게 되는지 알 수 있음
 
-const line='213';
-const a=parseInt(line/(10**len));
+// Run by Node.js
 
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
+rl.on("line", function(line) {
+	
+	const arr=line.split(' ');
 
-console.log(a);
+	const origin=parseInt(arr[0]);
+	const rate=parseFloat(arr[1])*0.01;
+	const years=parseInt(arr[2]);
+	
+	console.log((origin*(1+rate)**years).toFixed(2));
+	
+	rl.close();
+}).on("close", function() {
+	process.exit();
+});
